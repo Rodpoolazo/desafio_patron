55679cfa5bcf11c3490729ec517f5aa8a5545443
Rodpoolazo <rodpoolazo@gmail.com>
2019-04-14 22:55:38 -0400

    first commit

---
+++ desafio_patron2.rb
@@ -0,0 +1,11 @@
+# patron **..**
+
+n = ARGV[0].to_i
+
+n.times do |i|
+  if i%4 == 0 or i%4 == 1
+    print '*'
+  else
+    print "."
+  end
+end
---
+++ desafio_patron4.rb
@@ -0,0 +1,11 @@
+# patron 123123123
+
+n = ARGV[0].to_i
+
+n.times do |i|
+  if i.even?
+    print '12'
+  else
+    print "3"
+  end
+end
